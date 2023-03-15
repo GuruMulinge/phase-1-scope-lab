f5113fa99cf239992b299592109a146aa1b98872
Anthony Mulinge <anthony.mulinge@student.moringaschool.com>
2023-03-15 20:23:17 +0300

    Assignment Commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+var bestCustomer;
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+  
+
+const leastFavoriteCustomer = 'initial least favorite customer';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new least favorite customer';
+}
